dcf6842d1ebf144b64b3942e19f5e49873ecdbfc
Maria Mikhno <mmikhno89@gmail.com>
2024-07-17 19:16:01 +0300

    Second task

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,19 @@
 import java.util.*;
+import java.util.concurrent.*;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
         String[] texts = new String[25];
-        List<Thread> threads = new ArrayList<>();
-        Thread thread = null;
+        ExecutorService executorService = Executors.newFixedThreadPool(texts.length);
+        List<Future<Integer>> futureList = new ArrayList<Future<Integer>>();
         for (int i = 0; i < texts.length; i++) {
             texts[i] = generateText("aab", 30_000);
         }
-        long startTs = System.currentTimeMillis(); // start time
+
+        long startTs = System.currentTimeMillis();
         for (String text : texts) {
-            Runnable log = () -> {
+            Callable<Integer> log = () -> {
                 int maxSize = 0;
                 for (int i = 0; i < text.length(); i++) {
                     for (int j = 0; j < text.length(); j++) {
@@ -30,18 +32,30 @@ public class Main {
                         }
                     }
                 }
-                System.out.println(text.substring(0, 100) + " -> " + maxSize);
-            };
-            thread = new Thread(log);
-            thread.start();
-            threads.add(thread);
-        }
-        for (Thread t : threads) {
-            t.join();
-        }
-        long endTs = System.currentTimeMillis(); // end time
-        System.out.println("Time: " + (endTs - startTs) + "ms");
 
+                System.out.println(text.substring(0, 100) + " -> " + maxSize);
+                return maxSize;
+
+            };
+            Future future = executorService.submit(log);
+            futureList.add(future);
+        }
+        int maxVal = 0;
+        int result = 0;
+        for (Future<Integer> fut : futureList) {
+            try {
+                result = fut.get();
+                if (result > maxVal) {
+                    maxVal = result;
+                }
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(maxVal);
+        long endTs = System.currentTimeMillis();
+        System.out.println("Time: " + (endTs - startTs) + "ms");
+        executorService.shutdown();
     }
 
     public static String generateText(String letters, int length) {
